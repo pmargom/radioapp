41624831eb11536a296854fbd2501bd5f8a26985
Pedro Martin <pmargom@gmail.com>
2024-04-14 23:46:54 +0100

    Add colors and theme

---
+++ lib/core/config/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xff191B28);
+const fontColor = Color(0xffFFFFFF);
+const primaryColor = Color.fromARGB(255, 255, 0, 242);
+const divColor = Color(0xff34374B);
+const lableColor = Color(0xff9E9E9E);
