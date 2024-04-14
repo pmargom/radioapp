1f3d54de901f86a5176a9e73f09c4df34dd98116
Pedro Martin <pmargom@gmail.com>
2024-04-14 23:47:23 +0100

    update api mappings

--- lib/data/network/api_mappings.dart
+++ lib/data/network/api_mappings.dart
@@ -3,3 +3,4 @@ class ApiMappings {
 }
 
 // http://all.api.radio-browser.info/json/stations/bycountry/spain?limit=50&hidebroken=true
+// http://all.api.radio-browser.info/json/stations/byuuid?uuids=4a3bd909-1245-43bd-b77d-58053ad16f50
