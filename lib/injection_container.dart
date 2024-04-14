4b34ccbbc9904c239577ae1d0c4ac3ac651a0d66
Pedro Martin <pmargom@gmail.com>
2024-04-14 23:54:41 +0100

    Update dependency injections

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:radioapp/core/services/favourites_service.dart';
 import 'package:radioapp/data/datasources/remote_data_source.dart';
 import 'package:radioapp/data/datasources/remote_data_source_impl.dart';
 import 'package:radioapp/data/network/services_base_api.dart';
 import 'package:radioapp/data/repositories/radio_stations_repository_impl.dart';
 import 'package:radioapp/domain/repositories/radio_stations_repository.dart';
 import 'package:radioapp/domain/usecases/get_radio_station_usecase.dart';
+import 'package:radioapp/domain/usecases/get_radio_stations_usecase.dart';
+import 'package:radioapp/presentation/bloc/radio_station/radio_station_cubit.dart';
 import 'package:radioapp/presentation/bloc/radio_stations/radio_stations_cubit.dart';
 
 final getIt = GetIt.instance;
@@ -12,9 +15,11 @@ final getIt = GetIt.instance;
 Future<void> initDependencies() async {
   // Bloc
   getIt.registerFactory(() => RadioStationsCubit(getIt()));
+  getIt.registerFactory(() => RadioStationCubit(getIt()));
 
   // Use cases
   getIt.registerLazySingleton(() => GetRadioStationsUsecase(getIt()));
+  getIt.registerLazySingleton(() => GetRadioStationUsecase(getIt()));
 
   // Repositories
   getIt.registerLazySingleton<RadioStationsRepository>(() => RadioStationsRepositoryImpl(getIt()));
@@ -24,4 +29,5 @@ Future<void> initDependencies() async {
 
   // Api service
   getIt.registerLazySingleton(() => ServicesBaseApi());
+  getIt.registerLazySingleton(() => FavouritesService());
 }
